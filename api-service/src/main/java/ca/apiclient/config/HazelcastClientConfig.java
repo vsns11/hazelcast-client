01fa2f40e45b58bdc35377eca8f5337f22d81343
sivanagasaivemula <vemula.sivanagasai@gmail.com>
2024-04-24 13:07:28 -0400

    use hz client config

--- api-service/src/main/java/ca/apiclient/config/HazelcastClientConfig.java
+++ api-service/src/main/java/ca/apiclient/config/HazelcastClientConfig.java
@@ -10,18 +10,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HazelcastClientConfig {
 
-    @Autowired
-    private HazelcastProperties hazelcastProperties;
+
 
     @Bean
     public HazelcastInstance hazelcastInstance() {
-        ClientConfig clientConfig = new ClientConfig();
-        clientConfig.setClusterName(hazelcastProperties.getInstanceName());
-//        clientConfig.setProperty("zone", "SHIVA");
-//
-//        hazelcastProperties.getAddresses().forEach(address ->
-//            clientConfig.getNetworkConfig().addAddress(address));
 
-        return HazelcastClient.newHazelcastClient(clientConfig);
+        return HazelcastClient.newHazelcastClient();
     }
 }
